696ad6a48fd49c57a7ecb951d6502290068522a2
MikevEngelen <47856076+MikevEngelen@users.noreply.github.com>
2023-12-17 14:42:18 +0100

    Trying to connect to local mssql database

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const app = express();
 const PORT = 3000;
+// config for your database
+// const config = {
+//   user: 'DESKTOP-RESFGB5\mikev',
+//   password: '',
+//   server: '(localdb)\MSSQLLocalDB',
+//   database: 'moovd_gps_projectContext-c146eb5a-f487-41dd-b58f-5115aab599b4'
+// };
+const sql = require('mssql/msnodesqlv8');
+const conn = new sql.ConnectionPool({
+    database: 'moovd_gps_projectContext-c146eb5a-f487-41dd-b58f-5115aab599b4',
+    server: 'DESKTOP-RESFGB5\LOCALDB#3A01F028',
+    driver: 'msnodesqlv8',
+    options: {
+        trustedConnection: true
+    }
+});
 app.get('/', (req, res) => {
     res.send('Stofloos is the best!');
 });
@@ -18,10 +34,29 @@ const gps = [
         DeviceType: "Aircraft",
         TimeStamp: new Date().toTimeString(),
         Location: "L2"
+    },
+    {
+        DeviceId: 2,
+        DeviceType: "Personal",
+        TimeStamp: new Date().toTimeString(),
+        Location: "L3"
     }
 ];
 app.get('/api/gps', (req, res) => {
-    res.json(gps);
+    sql.connect(conn, function (err) {
+        if (err)
+            console.log(err);
+        // create Request object
+        var request = new sql.Request();
+        // query to the database and get the records
+        request.query('select * from dbo.GPS', function (err, recordset) {
+            if (err)
+                console.log(err);
+            // send records as a response
+            res.json(recordset);
+        });
+    });
+    //res.json(gps)
 });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,28 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// config for your database
+// const config = {
+//   user: 'DESKTOP-RESFGB5\mikev',
+//   password: '',
+//   server: '(localdb)\MSSQLLocalDB',
+//   database: 'moovd_gps_projectContext-c146eb5a-f487-41dd-b58f-5115aab599b4'
+// };
+const sql = require('mssql/msnodesqlv8')
+
+const conn = new sql.ConnectionPool({
+  database: 'moovd_gps_projectContext-c146eb5a-f487-41dd-b58f-5115aab599b4',
+  server: 'DESKTOP-RESFGB5\LOCALDB#3A01F028',
+  driver: 'msnodesqlv8',
+  options: {
+    trustedConnection: true
+  }
+})
+
 app.get('/', (req: any, res: any) => {
   res.send('Stofloos is the best!');
 });
+
 const gps: GPS[] = [
   {
     DeviceId: 0,
@@ -19,10 +38,35 @@ const gps: GPS[] = [
     DeviceType: "Aircraft",
     TimeStamp: new Date().toTimeString(),
     Location: "L2"
+  },
+  {
+    DeviceId: 2,
+    DeviceType: "Personal",
+    TimeStamp: new Date().toTimeString(),
+    Location: "L3"
   }
 ]
+
 app.get('/api/gps', (req: any, res: any) => {
-  res.json(gps)
+  sql.connect(conn, function (err: any) {
+
+    if (err) console.log(err);
+
+    // create Request object
+    var request = new sql.Request();
+
+    // query to the database and get the records
+    request.query('select * from dbo.GPS', function (err: any, recordset: any) {
+
+      if (err) console.log(err)
+
+      // send records as a response
+      res.json(recordset);
+
+    });
+  });
+
+  //res.json(gps)
 })
 
 
